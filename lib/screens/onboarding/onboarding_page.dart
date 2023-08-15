b26d6e946c9f38a4a844c7eb10e5b38db06a0d31
Undie Ebenezer <ebenezerundie@gmail.com>
2023-08-15 10:52:53 +0100

    feat: onboarding page UI

--- lib/screens/onboarding/onboarding_page.dart
+++ lib/screens/onboarding/onboarding_page.dart
@@ -1,3 +1,5 @@
+import 'package:design_task/helpers/constants.dart';
+import 'package:design_task/shared/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingPage extends StatefulWidget {
@@ -17,47 +19,111 @@ class _OnboardingPageState extends State<OnboardingPage> {
       body: Column(
         children: [
           Expanded(
-            child: Container(
-              color: Colors.red,
-              child: Stack(
-                alignment: const Alignment(0, -0.1),
+            flex: 4,
+            child: Stack(
+              alignment: const Alignment(0, -0.1),
+              children: [
+                Positioned(
+                  top: 0.04 * size.height,
+                  bottom: 0,
+                  right: 0,
+                  left: 0,
+                  child: Image.asset(
+                    'assets/images/onboarding/pattern.png',
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+                Positioned(
+                  bottom: -60,
+                  left: 0,
+                  right: size.width * 0.1,
+                  child: Image.asset(
+                    'assets/images/onboarding/line.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Image.asset('assets/images/onboarding/people.png')
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
                 children: [
-                  Positioned(
-                    top: 0.04 * size.height,
-                    bottom: 0,
-                    right: 0,
-                    left: 0,
-                    child: Image.asset(
-                      'assets/images/onboarding/pattern.png',
-                      fit: BoxFit.fitWidth,
+                  Container(
+                    margin: EdgeInsets.only(right: 0.15 * size.width),
+                    child: Text(
+                      'Send Money faster than Imagined',
+                      style: themeData.textTheme.bodyLarge!
+                          .copyWith(color: Colors.white),
                     ),
                   ),
-                  Positioned(
-                    bottom: -70,
-                    left: 0,
-                    right: size.width * 0.15,
-                    child: Image.asset(
-                      'assets/images/onboarding/line.png',
-                      fit: BoxFit.cover,
+                  const SizedBox(height: 5),
+                  Container(
+                    margin: EdgeInsets.only(right: 0.2 * size.width),
+                    child: Text(
+                      'Opticash provides you the fastest remittance to send and receive money!',
+                      style: themeData.textTheme.bodyMedium!.copyWith(
+                        color: const Color(0xFFCFCFCF),
+                      ),
                     ),
                   ),
-                  Image.asset('assets/images/onboarding/people.png')
+                  SizedBox(height: 0.043 * size.height),
+                  Row(
+                    children: [
+                      _progressIndicator(true),
+                      _progressIndicator(false),
+                      _progressIndicator(false),
+                    ],
+                  ),
+                  SizedBox(height: 0.04 * size.height),
+                  CustomButton(
+                    text: 'Create New Account',
+                    gradient: LinearGradient(
+                      end: const Alignment(1.00, -0.04),
+                      begin: const Alignment(-1, -0.04),
+                      colors: [
+                        livelyGreen,
+                        pastelYellow,
+                      ],
+                    ),
+                    textColor: black,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      print('tapped');
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30, vertical: 15),
+                      margin: const EdgeInsets.only(top: 10),
+                      child: Text(
+                        'Sign In',
+                        style: themeData.textTheme.bodyLarge!.copyWith(
+                          color: Colors.white,
+                          fontSize: 18,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  )
                 ],
               ),
             ),
           ),
-          Expanded(
-            child: Column(
-              children: [
-                Text(
-                  'Send Money faster than Imagined',
-                  style: themeData.textTheme.bodyLarge,
-                ),
-              ],
-            ),
-          ),
         ],
       ),
+    );
+  }
+
+  Widget _progressIndicator(bool isActive) {
+    return Container(
+      height: 2,
+      width: (MediaQuery.of(context).size.width - 72) / 5,
+      margin: const EdgeInsets.only(right: 8),
+      color: isActive ? const Color(0xFFC1D138) : const Color(0xFFA0A0A0),
     );
   }
 }
---
+++ lib/shared/custom_button.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final double radius;
+  final double? width;
+  final double height;
+  final Gradient? gradient;
+  final Color? color;
+  final String text;
+  final Color? textColor;
+  const CustomButton({
+    this.radius = 5,
+    this.width,
+    this.height = 60,
+    super.key,
+    this.gradient,
+    this.color,
+    required this.text,
+    this.textColor,
+  }) : assert(gradient != null || color != null,
+            'Either color or gradient must be provided.');
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData themeData = Theme.of(context);
+    return Container(
+      width: width ?? double.infinity,
+      alignment: Alignment.center,
+      height: height,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(radius),
+        gradient: gradient,
+        color: color,
+      ),
+      child: Text(
+        text,
+        style: themeData.textTheme.bodyLarge!
+            .copyWith(color: textColor, fontSize: 16),
+      ),
+    );
+  }
+}
